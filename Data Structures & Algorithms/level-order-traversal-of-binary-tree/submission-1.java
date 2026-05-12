21f1dc6a09ed40f915bbd5e10a973957f16e2a0f
Darwish <80089713+djquiambao18@users.noreply.github.com>
2026-05-11 23:47:14 -0700

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root != null) {
+            q.offer(root);
+            // could we utilize the property of base 2 powers (binary)
+            int n = q.size();
+            // loops here, outer loop condition is when queue is empty
+            // inner loop should check for queue size at the point in time which will know what level we are in
+            // 2^h property, where 'h' is the current height we are at, starting from '0' at root.
+            while(!q.isEmpty()) {
+                n = q.size();
+                // process the queue depending on the level:
+                int processed = 0;
+                List<Integer> list = new ArrayList<>();
+                for(int i = 0; i < n && q.peek() != null; i++) {
+                    TreeNode curr = q.poll();
+                    // insert into list:
+                    list.add(curr.val);
+                    if(curr.left != null) {
+                        q.offer(curr.left);
+                    }
+                    if(curr.right != null) {
+                        q.offer(curr.right);
+                    }
+                }
+                result.add(list);
+                n++;
+            }
+        }
+        return result;
+    }
+}
