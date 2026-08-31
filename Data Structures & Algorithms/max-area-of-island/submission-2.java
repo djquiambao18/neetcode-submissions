f651d542bc61ab0b9122ac5e6fbf59a11b05242c
Darwish <80089713+djquiambao18@users.noreply.github.com>
2026-08-30 22:37:23 -0700

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    // approach: num of islands but count max 1's
+    // dfs or bfs
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[row].length; col++) {
+                int val = grid[row][col];
+                if(val == 1) {
+                    max = Math.max(max, dfs(grid, row, col));
+                }
+            }
+        }
+        // this would be 0 if no island ever existed
+        return max;
+    }
+
+    private int dfs(int[][] grid, int row, int col) {
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0) {
+            return 0;
+        }
+        grid[row][col] = 0; // mark as visited
+        int res = 1;
+        for(int[] direction : directions) {
+            res += dfs(grid, row + direction[0], col + direction[1]);
+        }
+        return res;
+    }
+}
