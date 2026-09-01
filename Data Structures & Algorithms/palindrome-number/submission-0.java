f8565d93c8cfc6247eeca373d21081d01f07c029
Darwish <80089713+djquiambao18@users.noreply.github.com>
2026-08-31 22:16:08 -0700

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        if(temp == 0)
+            return true;
+        int reverse = 0;
+        // reverse the integer with modulo
+        // 123 % 10 => 3
+        // reverse = (reverse * 10) + 3
+        // reverse = (0) + 3
+        // 123 / 10 => 12
+        // 2. 12%10 => 2
+        // reverse = (3 * 10) + 2 -> reverse = (30) + 2 -> 32
+        // 12/10 => 1
+        // 3. 1%10 => 1
+        // reverse = 32*10 + 1 -> 321
+        while(temp != 0) {
+            // extract the last digit:
+            int last = temp % 10;
+            // ex1. reverse = (0 * 10) + 3
+            reverse = (reverse * 10) + last;
+            // 'remove' the last digit
+            temp /= 10;
+        }
+        return x == Math.abs(reverse);
+    }
+}
